6abb3381fe8df645abb4d0acb7ee4db8788ef62f
nerucheva <tatiana@nerucheva.com>
2024-05-08 20:38:21 +0200

    add Card and CardFeed elements

---
+++ src/components/CardFeed/index.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { Card } from '../Card';
+
+import styles from './styles.module.css';
+
+// TODO: fetch this data from somewhere outside
+const cardContent = [
+  {
+    image: 'src/assets/cardImg.png',
+    alt: 'A man in a stylish look stands stand with his back to the camera on a street',
+    category: 'Lifestyle',
+    title: 'Eat Right For Your Exercise Regime',
+    author: 'Niek Bove',
+    date: 'April 8, 2018',
+    views: '3K Views',
+    content: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+  },
+  {
+    image: 'src/assets/cardImg.png',
+    alt: 'A man in a stylish look stands stand with his back to the camera on a street',
+    category: 'Lifestyle',
+    title: 'Eat Right For Your Exercise Regime',
+    author: 'Niek Bove',
+    date: 'April 8, 2018',
+    views: '3K Views',
+    content: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+  },
+  {
+    image: 'src/assets/cardImg.png',
+    alt: 'A man in a stylish look stands stand with his back to the camera on a street',
+    category: 'Lifestyle',
+    title: 'Eat Right For Your Exercise Regime',
+    author: 'Niek Bove',
+    date: 'April 8, 2018',
+    views: '3K Views',
+    content: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+  },
+  {
+    image: 'src/assets/cardImg.png',
+    alt: 'A man in a stylish look stands stand with his back to the camera on a street',
+    category: 'Lifestyle',
+    title: 'Eat Right For Your Exercise Regime',
+    author: 'Niek Bove',
+    date: 'April 8, 2018',
+    views: '3K Views',
+    content: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+  },
+  {
+    image: 'src/assets/cardImg.png',
+    alt: 'A man in a stylish look stands stand with his back to the camera on a street',
+    category: 'Lifestyle',
+    title: 'Eat Right For Your Exercise Regime',
+    author: 'Niek Bove',
+    date: 'April 8, 2018',
+    views: '3K Views',
+    content: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+  },
+  {
+    image: 'src/assets/cardImg.png',
+    alt: 'A man in a stylish look stands stand with his back to the camera on a street',
+    category: 'Lifestyle',
+    title: 'Eat Right For Your Exercise Regime',
+    author: 'Niek Bove',
+    date: 'April 8, 2018',
+    views: '3K Views',
+    content: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+  },
+  {
+    image: 'src/assets/cardImg.png',
+    alt: 'A man in a stylish look stands stand with his back to the camera on a street',
+    category: 'Lifestyle',
+    title: 'Eat Right For Your Exercise Regime',
+    author: 'Niek Bove',
+    date: 'April 8, 2018',
+    views: '3K Views',
+    content: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+  },
+  {
+    image: 'src/assets/cardImg.png',
+    alt: 'A man in a stylish look stands stand with his back to the camera on a street',
+    category: 'Lifestyle',
+    title: 'Eat Right For Your Exercise Regime',
+    author: 'Niek Bove',
+    date: 'April 8, 2018',
+    views: '3K Views',
+    content: `Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…`,
+  },
+];
+
+export const CardFeed: React.FC = () => (
+  <div className={styles.cardContainer}>
+    {cardContent.map((item) => (
+      <Card alt={item.alt} image={item.image} category={item.category} title={item.title} author={item.author} date={item.date} views={item.views} content={item.content} />
+    ))}
+  </div>
+);
