cc05d82c05279491e780f0d30aa09e6ab3b2d703
nerucheva <tatiana@nerucheva.com>
2024-05-12 11:10:51 +0200

    Revert "optimise CardFeed"
    This reverts commit 3b0b5ecc6b09fc92df2396ee4b457f4f8ec43fec.
    
    # Conflicts:
    #	src/components/CardFeed/index.tsx

--- src/components/CardFeed/index.tsx
+++ src/components/CardFeed/index.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
-
 import { Card } from '../Card';
 import { Post } from '../Card';
 import styles from './styles.module.css';
@@ -9,10 +8,37 @@ type Props = {
   data: Post[];
 };
 
-export const CardFeed: React.FC<Props> = ({ data }) => (
-  <div className={clsx(styles.cardContainer, 'container')}>
-    {data.map((item) => (
-      <Card img={item.img} img_2x={item.img_2x} tags={item.tags} title={item.title} autor={item.autor} date={item.date} views={item.views} text={item.text} key={item.title} />
-    ))}
-  </div>
-);
+export const CardFeed: React.FC<Props> = ({ data }) => {
+  const [selectedCard, setSelectedCard] = useState<Post | null>(null);
+
+  const handleCardClick = (card: Post) => {
+    setSelectedCard(card);
+  };
+
+  return (
+    <>
+      <div className={clsx(styles.cardContainer, 'container')}>
+        {data.map((item) => (
+          <div className={styles.postButton} onClick={() => handleCardClick(item)} role="button" key={item.title}>
+            <Card img={item.img} img_2x={item.img_2x} tags={item.tags} title={item.title} autor={item.autor} date={item.date} views={item.views} text={item.text} />
+          </div>
+        ))}
+      </div>
+
+      {selectedCard && (
+        <div style={{ position: 'absolute', inset: 0, backgroundColor: 'pink', height: '100vh' }} onClick={() => setSelectedCard(null)} role="button">
+          <Card
+            img={selectedCard.img}
+            img_2x={selectedCard.img_2x}
+            tags={selectedCard.tags}
+            title={selectedCard.title}
+            autor={selectedCard.autor}
+            date={selectedCard.date}
+            views={selectedCard.views}
+            text={selectedCard.text}
+          />
+        </div>
+      )}
+    </>
+  );
+};
