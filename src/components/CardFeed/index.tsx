928b615c9cb1b9b690da3e63ec23cd3645b67bb2
nerucheva <tatiana@nerucheva.com>
2024-05-12 14:45:56 +0200

    refactoring

--- src/components/CardFeed/index.tsx
+++ src/components/CardFeed/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import clsx from 'clsx';
+
 import { Card } from '../Card';
 import { Post } from '../Card';
+import { Modal } from '../Modal';
+
 import styles from './styles.module.css';
 
 type Props = {
@@ -23,20 +26,22 @@ export const CardFeed: React.FC<Props> = ({ data }) => {
         ))}
       </div>
 
-      {selectedCard && (
-        <div style={{ position: 'absolute', inset: 0, backgroundColor: 'pink', height: '100vh' }} onClick={() => setSelectedCardIndex(undefined)} role="button">
-          <Card
-            img={selectedCard.img}
-            img_2x={selectedCard.img_2x}
-            tags={selectedCard.tags}
-            title={selectedCard.title}
-            autor={selectedCard.autor}
-            date={selectedCard.date}
-            views={selectedCard.views}
-            text={selectedCard.text}
-          />
-        </div>
-      )}
+      <Modal onClose={() => setSelectedCardIndex(undefined)} isVisible={!!selectedCard}>
+        {selectedCard && (
+          <div className={clsx(styles.cardWrapper, 'container')}>
+            <Card
+              img={selectedCard.img}
+              img_2x={selectedCard.img_2x}
+              tags={selectedCard.tags}
+              title={selectedCard.title}
+              autor={selectedCard.autor}
+              date={selectedCard.date}
+              views={selectedCard.views}
+              text={selectedCard.text}
+            />
+          </div>
+        )}
+      </Modal>
     </>
   );
 };
