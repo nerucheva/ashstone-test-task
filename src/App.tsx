7f1e17d29250440582dd80a21e9eae1e398f3ea3
nerucheva <tatiana@nerucheva.com>
2024-05-13 12:49:04 +0200

    fix search

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { useState, useEffect } from 'react';
-
 import { Header } from './components/Header';
 import { Menu } from './components/Menu';
 import { CardFeed } from './components/CardFeed';
 import { MobileMenuModal } from './components/MobileMenuModal';
-
 import { Post } from './components/Card';
-
 import './App.css';
 
 function App() {
   const [isVisible, setIsVisible] = useState(false);
   const [query, setQuery] = useState('');
   const [postsList, setPostsList] = useState<Post[]>([]);
+  const [filteredPosts, setFilteredPosts] = useState<Post[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,34 +27,23 @@ function App() {
   }, []);
 
   useEffect(() => {
-    const Debounce = setTimeout(() => {
-      const filteredCars = filterPosts(query, postsList);
-      setPostsList(filteredCars);
+    const debounce = setTimeout(() => {
+      const filteredPosts = postsList.filter((post) => post.title.toLowerCase().includes(query.toLowerCase()) || post.text.toLowerCase().includes(query.toLowerCase()));
+      setFilteredPosts(filteredPosts);
     }, 300);
 
-    return () => clearTimeout(Debounce);
+    return () => clearTimeout(debounce);
   }, [query, postsList]);
-
-  const filterPosts = (searchText: string, postsList: Post[]) => {
-    if (!searchText) {
-      return postsList;
-    }
-    return postsList.filter(({ title, text }) => title.toLowerCase().includes(searchText.toLowerCase()) || text.toLowerCase().includes(searchText.toLowerCase()));
-  };
 
   return (
     <>
       <Header onClick={() => setIsVisible(true)} searchOnChange={(e) => setQuery(e.target.value)} />
-
       <main>
         <Menu />
-
         <MobileMenuModal onClose={() => setIsVisible(false)} isVisible={isVisible} />
-
         <h1 className="visuallyHidden">Home page</h1>
-
         <section className="cardFeedWrapper">
-          <CardFeed data={postsList} />
+          <CardFeed data={filteredPosts} />
         </section>
       </main>
     </>
